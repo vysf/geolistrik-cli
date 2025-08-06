63d220011939a07c87d1486756998d68c0ee9fe1
vysf <yusufumar@student.untan.ac.id>
2025-08-06 14:34:23 +0700

    adding functions for secondry axis matplotlib

--- geolistrik/utils/utils.py
+++ geolistrik/utils/utils.py
@@ -10,3 +10,13 @@ def mapping_by_index(conf_elctrode, elektrode):
     elektroda_index_map = {value: idx for idx, value in enumerate(elektrode)}
     indeks = [elektroda_index_map[ce] for ce in conf_elctrode]
     return np.array(indeks)
+
+# Fungsi mapping untuk secondary x-axis
+
+def make_position_to_index(a):
+    def position_to_index(x): return x / a
+    return position_to_index
+
+def make_index_to_position(a):
+    def index_to_position(x): return x * a
+    return index_to_position
